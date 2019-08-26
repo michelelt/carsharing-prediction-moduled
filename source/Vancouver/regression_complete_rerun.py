762f51a587f497cd7c56cf56e38001a6bb84015e
michele <michelecocca.mc@gmail.com>
2019-08-26 17:42:30 +0200

    new global run script

---
+++ source/Vancouver/regression_complete_rerun.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 26 17:38:06 2019
+
+@author: mc
+"""
+
+import sys
+import os
+
+os.system('python3 regression_moduled.py')
+os.system('python3 regression_moduled_fs.py rfr & python3 regression_moduled_fs.py svr')
